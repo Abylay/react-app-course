da01f642eb888a631229ee8b2abc6faa348f56ad
Abylay <boranbayev.abylay@gmail.com>
2026-04-17 21:16:08 +0500

    react basics

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,45 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+import react from "eslint-plugin-react";
+import prettier from "eslint-plugin-prettier";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{js,jsx}"],
+    plugins: {
+      react,
+      prettier,
+    },
+    extends: [js.configs.recommended, reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "react/jsx-no-target-blank": "warn",
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: false,
+          printWidth: 130,
+          tabWidth: 2,
+        },
+      ],
     },
   },
-])
+]);
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import cls from "./Button.module.css";
+
+const isPrimary = true;
+
+export const Button = (props) => {
+  console.log(props);
+  return (
+    // <button className={isPrimary ? cls.primary : cls.btn}>Button</button>
+    <button className={`${cls.btn} ${isPrimary ? cls.primary : ""}`} onClick={props.onClick}>
+      {props.text}
+    </button>
+  );
+};
